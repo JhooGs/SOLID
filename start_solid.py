884b88d7b8c9ed8ba4f22ec1c3a7fdabc9895873
JonathanZen <jonathan.goncalves@zenklub.com>
2023-09-04 21:49:49 -0300

    start learning

---
+++ start_solid.py
@@ -0,0 +1,8 @@
+# S
+# Principio da Responsabilidade Única
+# Sua classe não deve ter mais de uma responsabilidade
+# Facilidade de manutenção e evolução do código
+# Código limpo e claro
+# Facilidade de desenvolver testes
+# Reduz o acoplamento
+
