16e7deacf47da0c4decde0e7ca3d1fb4b61ebf4a
JonathanGL8 <jonathan.santo@l8automation.com>
2023-10-16 12:11:11 -0300

    consolidando conhecimentos sobre S do solid

--- bad_srp.py
+++ bad_srp.py
@@ -1,20 +1,9 @@
-import requests
 
 
-class ListRepositories:
 
-    API_BASE_URL = 'https://api.github.com'
 
-    def __init__(self, user):
-        self.user = user
 
-    def get_repos_by_user(self):
-        response = requests.get(f'{self.API_BASE_URL}/users/{self.user}/repos')
 
-        if response.status_code == 200:
-            return {"status_code": 200, "body": response.json()}
-        else:
-            return {"status_code": response.status_code, "body": "Error while getting repos"}
 
     def parse_response(self):
         response = self.get_repos_by_user()
